0d8c1e8338519af6ab402d8baf8c9258272a9e51
dbgee <46910972+dbgee@users.noreply.github.com>
2022-05-23 18:37:44 +0800

    add category module

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,72 +1,80 @@
-// pages/category/category.js
 Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {},
-
-  getData() {
-    console.log("点击getdata");
-    var reqTask = wx.request({
-      url: "https://mall.meituan.com/api/c/poi/10000054/sku/list/category/1003/v6?uuid=000000000000042D1B59CFBDC485DB1737A984EDEFC1BA165313238001202228&utm_term=5.34.0&version_name=5.34.10&poi=10000054&optimusCode=10&userid=3660557181&utm_medium=iphone&tenantId=1&platform=ios&__reqTraceID=CBF21DE7-5AF7-427A-B9E1-8625672F410C&uci=2&riskLevel=71&ci=2&xuuid=759ad623edfb4f85d18aaca2d99195274fcd1779563b3cf03e&app_tag=union&language=zh_CN&limit=100&skuId=-1&sortType=0&isDefaultPoi=false&app_trace=6694deb754466ffc0d71fd48a7c46358&isShowNewCusAndSecArea=false&utm_content=000000000000042D1B59CFBDC485DB1737A984EDEFC1BA165313238001202228&utm_source=AppStore&type=4&token=0Sdfkvoa09Lhup9BLq7J1BzHd4EAAAAA4hEAAPWfdS9uTon3mAtZ2s07cQ6RQSK2sJYIJRyOeOrg-5P80C1tNjFoEoftpnrrWF8l4g&offset=0&listTag=1&firstCategoryId=40488&utm_campaign=Aimaicai_cBimaicai_cD200H0",
-      data: {},
-      header: {
-        "content-type": "application/json",
-        "T": "0Sdfkvoa09Lhup9BLq7J1BzHd4EAAAAA4hEAAPWfdS9uTon3mAtZ2s07cQ6RQSK2sJYIJRyOeOrg-5P80C1tNjFoEoftpnrrWF8l4g"
+  data: {
+    category:[],
+    activeItem:'',
+    active:"",
+    itemNumber:1,
+    products: [
+      {
+        id: 1,
+        title: "大白菜普通（水洗）",
+        price: 2.35,
+        promotion: "比加入时便宜0.52元",
+        itemNumber: 1,
+        selected: false,
       },
-      method: "GET",
-      dataType: "json",
-      responseType: "text",
-      success: (result) => {
-        console.log("header=" + result.header["Server"]);
-        console.log("status=" + result.statusCode);
-
-        console.log("result=" + result.data.data);
-        var str=JSON.stringify(result.data.data.skuId2SkuItemMap)
-        console.log('str='+str);
+      {
+        id: 2,
+        title: "调料",
+        price: 5.5,
+        promotion: "比加入时便宜0.2元",
+        itemNumber: 1,
+        selected: false,
       },
-      fail: () => {},
-      complete: () => {},
+      {
+        id: 3,
+        title: "新鲜芹菜",
+        price: 2.3,
+        promotion: "比加入时便宜0.5元",
+        itemNumber: 1,
+        selected: false,
+      },
+    ],
+  },
+
+  selectItem(e){
+    var id=e.target.dataset.id;
+    console.log('id='+id);
+    this.setData({
+      activeItem:id
+    })
+  },
+
+  selectProduct(e){
+    var index=e.target.dataset.index
+    console.log(index);
+    this.setData({
+      ["products[" + index + "].select"]: true,
+    })
+    
+  },
+
+  changeItemNum(e) {
+    var data = e.target.dataset;
+    var newNum = data.num;
+    var item = data.item;
+    var index = data.index;
+
+    if (newNum < 0) {
+      newNum = 0;
+    }
+
+    item.itemNumber = newNum;
+    this.setData({
+      ["products[" + index + "].itemNumber"]: newNum,
     });
   },
+  
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {},
+  onLoad(options) {
+    var appInst =  getApp();
+    var category=wx.getStorageSync("category");
+    this.setData({
+      category,
+    })
+  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
 });
--- pages/me/me.js
+++ pages/me/me.js
@@ -1,125 +1,119 @@
 // pages/me/me.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    temp:{},
-    login_btnName:'微信一键登录'
+    temp: {},
+    login_btnName: "微信一键登录",
   },
 
-  onLoad(){
-    var appInst =  getApp();
-    var login_btnName=appInst.globalData.login_btnName;
+  onLoad() {
+    var appInst = getApp();
+    var login_btnName = appInst.globalData.login_btnName;
     this.setData({
-      login_btnName
-    })
-  },
-
-  onShow(){
-    var appInst =  getApp();
-    var login_btnName=appInst.globalData.login_btnName;
-    this.setData({
-      login_btnName
-    })
-  },
-
-  toLogout(){
-    var that=this
-    var login_btnName="微信一键登录"
-    getApp().globalData.login_btnName=login_btnName
-    wx.showModal({
-      content: '确认是否退出登录？',
-      showCancel: true,
-      cancelText: '取消',
-      cancelColor: '#000000',
-      confirmText: '确定',
-      confirmColor: '#3CC51F',
-      success: (result) => {
-        if(result.confirm){
-          console.log('已退出登录');
-          that.setData({
-            login_btnName
-          })
-        }
-      },
-      fail: ()=>{},
-      complete: ()=>{}
+      login_btnName,
     });
-    
   },
 
-  scan(){
-    wx.scanCode({
-      onlyFromCamera: false,
-      scanType: ['qrCode', 'barCode', 'datamatrix', 'pdf417'],
+  onShow() {
+    var appInst = getApp();
+    var login_btnName = appInst.globalData.login_btnName;
+    this.setData({
+      login_btnName,
+    });
+  },
+
+  toLogout() {
+    var that = this;
+    var login_btnName = "微信一键登录";
+    getApp().globalData.login_btnName = login_btnName;
+    wx.showModal({
+      content: "确认是否退出登录？",
+      showCancel: true,
+      cancelText: "取消",
+      cancelColor: "#000000",
+      confirmText: "确定",
+      confirmColor: "#3CC51F",
       success: (result) => {
-        wx.showToast({
-          title: '扫码成功',
-          icon: 'none',
-          image: '',
-          duration: 700,
-          mask: false,
-          
-          success: (temp) => {
-            
-          },
-          fail: () => {},
-          complete: () => {}
-        });
-          
+        if (result.confirm) {
+          console.log("已退出登录");
+          wx.setStorageSync("login", false);
+
+          that.setData({
+            login_btnName,
+          });
+        }
       },
       fail: () => {},
-      complete: () => {}
+      complete: () => {},
     });
-      
   },
-  toLogin(){
-    if(getApp().globalData.login_btnName=="已登录"){
+
+  scan() {
+    wx.scanCode({
+      onlyFromCamera: false,
+      scanType: ["qrCode", "barCode", "datamatrix", "pdf417"],
+      success: (result) => {
+        wx.showToast({
+          title: "扫码成功",
+          icon: "none",
+          image: "",
+          duration: 700,
+          mask: false,
+
+          success: (temp) => {},
+          fail: () => {},
+          complete: () => {},
+        });
+      },
+      fail: () => {},
+      complete: () => {},
+    });
+  },
+  toLogin() {
+    if (getApp().globalData.login_btnName == "已登录") {
       wx.showModal({
-        title: '',
-        content: '您已登录，无需重复登录',
+        title: "",
+        content: "您已登录，无需重复登录",
         showCancel: false,
-        confirmText: '我知道了',
-        confirmColor: '#3CC51F',
+        confirmText: "我知道了",
+        confirmColor: "#3CC51F",
       });
-    }else{
+    } else {
       wx.navigateTo({
-        url: '/pages/login/login',
+        url: "/pages/login/login",
       });
     }
-    
-      
   },
-  setting(){
+  setting() {
     wx.openSetting({
-      success: (result)=>{
-        if(!result.authSetting['scope.record']){
-          console.log('data='+result.authSetting['scope.record']);
+      success: (result) => {
+        if (!result.authSetting["scope.record"]) {
+          console.log("data=" + result.authSetting["scope.record"]);
           wx.authorize({
-            scope: 'scope.record',
-            success: (result)=>{
-              console.log('成功获取麦克风权限');
+            scope: "scope.record",
+            success: (result) => {
+              console.log("成功获取麦克风权限");
             },
-            fail: ()=>{},
-            complete: ()=>{}
+            fail: () => {},
+            complete: () => {},
           });
         }
 
-        if(!result.authSetting['scope.camera']){
+        if (!result.authSetting["scope.camera"]) {
           wx.authorize({
-            scope: 'scope.camera',
-            success: (result)=>{
-              console.log('成功获取摄像头权限')
+            scope: "scope.camera",
+            success: (result) => {
+              console.log("成功获取摄像头权限");
             },
-            fail: ()=>{},
-            complete: ()=>{}
+            fail: () => {},
+            complete: () => {},
           });
         }
       },
-      fail: ()=>{},
-      complete: ()=>{}
+      fail: () => {},
+      complete: () => {},
     });
-  }
-})
+  },
+});
